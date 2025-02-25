c707aae504e0d1f224b34e4079a6c5fc83e01f05
Yuxuan Zhou <yuz235@ucsd.edu>
2025-02-25 13:19:16 -0800

    Update global.js

--- global.js
+++ global.js
@@ -135,6 +135,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
   }
   containerElement.innerHTML = '';
   for (let project of projects) {
+    console.log("2${project.image}");
     const article = document.createElement('article');
     article.innerHTML = `
     <${headingLevel}>
@@ -144,7 +145,6 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     <p>${project.description}</p>
     <div>Year: ${project.year}</div>
     `;
-    console.log("${project.image}");
     containerElement.appendChild(article);
   }
 }
